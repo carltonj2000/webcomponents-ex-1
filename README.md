e22f67f4d4074d6eee4a8526cbdaf3c46add8a7a
Carlton Joseph <carlton.joseph@gmail.com>
2022-04-20 21:14:57 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Web Components Example 1
+
+The code in this repository is based on the
+[Intro to Web Components](https://youtu.be/PFpUCnyztJk)
+video.
---
+++ app.js
@@ -0,0 +1,89 @@
+class BlogPost extends HTMLElement {
+  #description = "";
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.render();
+  }
+
+  connectedCallback() {
+    console.log("-- mounted");
+    // this.render(); // if want called before constructor
+  }
+
+  disconnectedCallback() {
+    console.log("-- unmounted");
+  }
+
+  set description(value) {
+    this.#description = value;
+  }
+
+  get description() {
+    return this.#description;
+  }
+
+  set data(value) {
+    console.log("-- value", value);
+    this.titleElement.textContent ||= value.title;
+    this.descriptionElement.textContent = value.description || "";
+    this.linkElement.textContent = value.link || "";
+  }
+
+  static get observedAttributes() {
+    return ["title", "description", "link"];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log("-- att changed", name, oldValue, newValue);
+    switch (name) {
+      case "title":
+        // this.title = newValue || ""; // with render() call below
+        this.titleElement.textContent = newValue || ""; // no render() more efficient
+        break;
+      case "description":
+        this.descriptionElement.textContent = newValue || "";
+        break;
+      case "link":
+        this.linkElement.textContent = newValue || "";
+        break;
+    }
+    // this.render();
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = /* html */ `
+      <div class="blog-post">
+        <h2>${this.title}</h2>
+        <p>${this.description}</p>
+        <a href="">${this.link}</a>
+      </div>`;
+
+    this.titleElement = this.shadowRoot.querySelector("h2");
+    this.descriptionElement = this.shadowRoot.querySelector("p");
+    this.linkElement = this.shadowRoot.querySelector("a");
+    console.log(
+      "tdl",
+      this.titleElement,
+      this.descriptionElement,
+      this.linkElement
+    );
+  }
+}
+
+customElements.define("blog-post", BlogPost);
+
+{
+  const post = document.querySelector("blog-post");
+  post.shadowRoot.querySelector(".blog-post").style.background = "#f00";
+
+  post.data = {
+    title: "Web Components",
+    link: "learn more",
+    description:
+      "Qui enim dolore veniam commodo occaecat duis cillum laborum ea laborum.",
+  };
+  // setTimeout(() => post.remove(), 2500); // test disconnectCallback
+  setTimeout(() => post.removeAttribute("title"), 1500);
+}
